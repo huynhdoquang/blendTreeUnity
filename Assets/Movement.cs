1bce0be8f207adaefc5c336867444a55caaa9385
huynh_do <huynh.fetel@gmail.com>
2018-08-30 18:20:37 +0700

    first init

---
+++ Assets/Movement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    Vector3 heading;
+    public float moveSpeed = 4f;
+
+    Animator thisAnim;
+    float lastX, lastY;
+
+    float horizontal;
+    float vertical;
+
+    public GameObject bulletPrefab;
+    // Use this for initialization
+    void Start () {
+        thisAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            Shoot();
+        }
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 rightMovement = Vector3.right * moveSpeed *Time.deltaTime * Input.GetAxisRaw("Horizontal");
+        Vector3 upMovement = Vector3.up * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+        UpdateAnimation(heading);
+    }
+
+    void UpdateAnimation(Vector3 dir)
+    {
+        if(dir.x == 0 && dir.y == 0)
+        {
+            thisAnim.SetFloat("LastDirX", lastX);
+            thisAnim.SetFloat("LastDirY", lastY);
+            thisAnim.SetBool("Movement", false);
+        }
+        else
+        {
+            lastX = dir.x;
+            lastY = dir.y;
+            thisAnim.SetBool("Movement", true);
+        }
+
+        thisAnim.SetFloat("DirX", dir.x);
+        thisAnim.SetFloat("DirY", dir.y);
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Lean.Pool.LeanPool.Spawn(bulletPrefab);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = transform.rotation;
+    }
+}
